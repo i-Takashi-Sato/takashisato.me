12d5f8d31166a126f2ccf7573c8ae8ec9dde1958
i-Takashi-Sato <i@takashisato.me>
2026-02-03 07:00:21 +0900

    Update altrion-part2.js

--- demo/_assets/altrion-part2.js
+++ demo/_assets/altrion-part2.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 const clamp01 = (x) => Math.min(1, Math.max(0, x));
 const mix = (a, b, t) => a * (1 - t) + b * t;
